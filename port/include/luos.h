f99985be9d60dd564c4989ebc85bb99629b24ab9
Mathis Lécrivain <mathis.lecrivain@rtone.fr>
2022-05-31 16:53:03 +0200

    feat: add CmakeList.txt component file

---
+++ port/include/luos.h
@@ -0,0 +1,27 @@
+/**
+ * @file luos.h
+ * @author Mathis Lécrivain 
+ * @brief Luos port inclusions 
+ * @date 2022-06-16 (Creation)
+ * 
+ * @copyright (c) 2022
+ * 
+ */
+#ifndef INC_LUOS_H
+#define INC_LUOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "luos_engine.h"
+
+#include "gate.h"
+#include "inspector.h"
+#include "pipe.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_LUOS_H */
